9db0e83ff839007f0e000943d852c120d9f04824
Gustavo Palla <pallagp34@gmail.com>
2026-03-31 14:19:26 -0300

    add: search_page

--- lib/features/search/bloc/bloc/search_bloc.dart
+++ lib/features/search/bloc/bloc/search_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_project_app/data/models/weather_model.dart';
 import 'package:weather_project_app/data/repositories/weather_repository.dart';
 
