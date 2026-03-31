8bb89052049853168e7d9a660c75c9f76854e833
Gustavo Palla <pallagp34@gmail.com>
2026-03-31 12:02:29 -0300

    add: search_state.dart

--- lib/features/search/bloc/bloc/search_state.dart
+++ lib/features/search/bloc/bloc/search_state.dart
@@ -2,9 +2,27 @@ part of 'search_bloc.dart';
 
 sealed class SearchState extends Equatable {
   const SearchState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class SearchInitial extends SearchState {}
+
+final class SearchLoading extends SearchState {}
+
+final class SearchSucess extends SearchState {
+  final WeatherModel weather;
+  const SearchSucess({required this.weather});
+
+  @override
+  List<Object> get props => [weather];
+}
+
+final class SearchFailure extends SearchState {
+  final String errorMessage;
+  const SearchFailure({required this.errorMessage});
+
+  @override
+  List<Object> get props => [errorMessage];
+}
